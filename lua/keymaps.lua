74805f84d47cabbe8d27b055b6a97eed2d016ba5
Klysmus <Klysmus@online.de>
2024-04-20 22:06:22 +0200

    Added useful keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -7,6 +7,11 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Set keymap for folder mode
 vim.keymap.set('n', '<leader>F', vim.cmd.Ex, { desc = 'Go into [F]ile system mode' })
+
+-- Set move marked lines
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
