d822aab28278ce4344bf1ae889ab2c56bfb12e54
Klysmus <Klysmus@online.de>
2024-04-28 21:09:03 +0200

    Add new keymaps to vimtex

--- lua/custom/plugins/vimtex.lua
+++ lua/custom/plugins/vimtex.lua
@@ -6,5 +6,8 @@ return {
     -- vim.cmd("set conceallevel=1")
     vim.keymap.set('n', '<leader>tc', '<cmd>VimtexCompile<CR>', { desc = '[c]ompile the document' })
     vim.keymap.set('n', '<leader>tv', '<cmd>VimtexView<CR>', { desc = '[v]iew this line' })
+    vim.keymap.set('n', '<leader>ti', '<cmd>VimtexTocOpen<CR>', { desc = '[I]ndex' })
+    vim.keymap.set('n', '<leader>tr', '<cmd>VimtexClean<CR>', { desc = '[R]emove the aux' })
+    vim.keymap.set('n', '<leader>tm', '<cmd>VimtexContextMenu<CR>', { desc = 'Context [m]enu' })
   end,
 }
