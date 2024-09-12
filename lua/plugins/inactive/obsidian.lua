7fccd2ee0df23aefee4cb4517434330df5f4d350
Klysmus <Klysmus@online.de>
2024-09-12 15:17:24 +0200

    Uncomment obsidian plugin

--- lua/plugins/inactive/obsidian.lua
+++ lua/plugins/inactive/obsidian.lua
@@ -1,106 +1,106 @@
--- -- You can add your own plugins here or in other files in this directory!
--- --  I promise not to create any merge conflicts in this directory :)
--- --
--- -- See the kickstart.nvim README for more information
--- return {
---   'epwalsh/obsidian.nvim',
---   version = '*', -- recommended, use latest release instead of latest commit
---   lazy = true,
---   ft = 'markdown',
---   --  vim.opt.conceallevel = 2,
---   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
---   -- event = {
---   --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
---   --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
---   --   "BufReadPre path/to/my-vault/**.md",
---   --   "BufNewFile path/to/my-vault/**.md",
---   -- },
---   dependencies = {
---     -- Required.
---     'nvim-lua/plenary.nvim',
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
 --
---     -- see below for full list of optional dependencies 👇
---   },
---   opts = {
---     workspaces = {
---       {
---         name = 'personal',
---         path = '~/Dokumente/Personel',
---         overrides = {
---           daily_notes = {
---             folder = 'Tagebuch/Täglich/thisYear',
---             date_format = '%Y-%m-%d',
---             template = nil,
---           },
---           attachments = {
---             img_folder = 'Attachments',
---           },
---         },
---       },
---       {
---         name = 'work',
---         path = '~/Dokumente/DND',
---       },
---     },
---
---     -- see below for full list of options 👇
---     new_notes_location = 'Verschiedenes/',
---     note_id_func = function(title)
---       -- Fuck Ids
---       -- In this case a note with the title 'My new note' will be given an ID that looks
---       -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
---       local suffix = '📒'
---       if title ~= nil then
---         -- If title is given, transform it into valid file name.
---         suffix = title:gsub(' ', '-'):gsub('[^A-Za-z0-9-]', ''):lower()
---       else
---         -- If title is nil, just add 4 random uppercase letters to the suffix.
---         for _ = 1, 4 do
---           suffix = suffix .. string.char(math.random(65, 90))
---         end
---       end
---       return '📒' .. suffix
---     end,
---     preferred_link_style = 'Markdown',
---     ui = {
---       enable = true, -- set to false to disable all additional syntax features
---       update_debounce = 200, -- update delay after a text change (in milliseconds)
---       -- Define how various check-boxes are displayed
---       checkboxes = {
---         -- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
---         -- [' '] = { char = '󰄱', hl_group = 'ObsidianTodo' },
---         -- ['x'] = { char = '', hl_group = 'ObsidianDone' },
---         -- ['>'] = { char = '', hl_group = 'ObsidianRightArrow' },
---         -- ['~'] = { char = '󰰱', hl_group = 'ObsidianTilde' },
---         -- Replace the above with this if you don't have a patched font:
---         [' '] = { char = '☐', hl_group = 'ObsidianTodo' },
---         ['x'] = { char = '✔', hl_group = 'ObsidianDone' },
---         ['X'] = { char = '✔', hl_group = 'ObsidianDone' },
---
---         -- You can also add more custom ones...
---       },
---       -- Use bullet marks for non-checkbox lists.
---       bullets = { char = '•', hl_group = 'ObsidianBullet' },
---       -- external_link_icon = { char = '', hl_group = 'ObsidianExtLinkIcon' },
---       -- Replace the above with this if you don't have a patched font:
---       external_link_icon = { char = '', hl_group = 'ObsidianExtLinkIcon' },
---       reference_text = { hl_group = 'ObsidianRefText' },
---       highlight_text = { hl_group = 'ObsidianHighlightText' },
---       tags = { hl_group = 'ObsidianTag' },
---       block_ids = { hl_group = 'ObsidianBlockID' },
---       hl_groups = {
---         -- The options are passed directly to `vim.api.nvim_set_hl()`. See `:help nvim_set_hl`.
---         ObsidianTodo = { bold = true, fg = '#f78c6c' },
---         ObsidianDone = { bold = true, fg = '#89ddff' },
---         ObsidianRightArrow = { bold = true, fg = '#f78c6c' },
---         ObsidianTilde = { bold = true, fg = '#ff5370' },
---         ObsidianBullet = { bold = true, fg = '#89ddff' },
---         ObsidianRefText = { underline = true, fg = '#c792ea' },
---         ObsidianExtLinkIcon = { fg = '#c792ea' },
---         ObsidianTag = { italic = true, fg = '#89ddff' },
---         ObsidianBlockID = { italic = true, fg = '#89ddff' },
---         ObsidianHighlightText = { bg = '#75662e' },
---       },
---     },
---   },
--- }
+-- See the kickstart.nvim README for more information
+return {
+  'epwalsh/obsidian.nvim',
+  version = '*', -- recommended, use latest release instead of latest commit
+  lazy = true,
+  ft = 'markdown',
+  --  vim.opt.conceallevel = 2,
+  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+  -- event = {
+  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+  --   "BufReadPre path/to/my-vault/**.md",
+  --   "BufNewFile path/to/my-vault/**.md",
+  -- },
+  dependencies = {
+    -- Required.
+    'nvim-lua/plenary.nvim',
+
+    -- see below for full list of optional dependencies 👇
+  },
+  opts = {
+    workspaces = {
+      {
+        name = 'personal',
+        path = '~/Dokumente/Personel',
+        overrides = {
+          daily_notes = {
+            folder = 'Tagebuch/Täglich/thisYear',
+            date_format = '%Y-%m-%d',
+            template = nil,
+          },
+          attachments = {
+            img_folder = 'Attachments',
+          },
+        },
+      },
+      {
+        name = 'work',
+        path = '~/Dokumente/DND',
+      },
+    },
+
+    -- see below for full list of options 👇
+    new_notes_location = 'Verschiedenes/',
+    note_id_func = function(title)
+      -- Fuck Ids
+      -- In this case a note with the title 'My new note' will be given an ID that looks
+      -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+      local suffix = '📒'
+      if title ~= nil then
+        -- If title is given, transform it into valid file name.
+        suffix = title:gsub(' ', '-'):gsub('[^A-Za-z0-9-]', ''):lower()
+      else
+        -- If title is nil, just add 4 random uppercase letters to the suffix.
+        for _ = 1, 4 do
+          suffix = suffix .. string.char(math.random(65, 90))
+        end
+      end
+      return '📒' .. suffix
+    end,
+    preferred_link_style = 'Markdown',
+    ui = {
+      enable = true, -- set to false to disable all additional syntax features
+      update_debounce = 200, -- update delay after a text change (in milliseconds)
+      -- Define how various check-boxes are displayed
+      checkboxes = {
+        -- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
+        -- [' '] = { char = '󰄱', hl_group = 'ObsidianTodo' },
+        -- ['x'] = { char = '', hl_group = 'ObsidianDone' },
+        -- ['>'] = { char = '', hl_group = 'ObsidianRightArrow' },
+        -- ['~'] = { char = '󰰱', hl_group = 'ObsidianTilde' },
+        -- Replace the above with this if you don't have a patched font:
+        [' '] = { char = '☐', hl_group = 'ObsidianTodo' },
+        ['x'] = { char = '✔', hl_group = 'ObsidianDone' },
+        ['X'] = { char = '✔', hl_group = 'ObsidianDone' },
+
+        -- You can also add more custom ones...
+      },
+      -- Use bullet marks for non-checkbox lists.
+      bullets = { char = '•', hl_group = 'ObsidianBullet' },
+      -- external_link_icon = { char = '', hl_group = 'ObsidianExtLinkIcon' },
+      -- Replace the above with this if you don't have a patched font:
+      external_link_icon = { char = '', hl_group = 'ObsidianExtLinkIcon' },
+      reference_text = { hl_group = 'ObsidianRefText' },
+      highlight_text = { hl_group = 'ObsidianHighlightText' },
+      tags = { hl_group = 'ObsidianTag' },
+      block_ids = { hl_group = 'ObsidianBlockID' },
+      hl_groups = {
+        -- The options are passed directly to `vim.api.nvim_set_hl()`. See `:help nvim_set_hl`.
+        ObsidianTodo = { bold = true, fg = '#f78c6c' },
+        ObsidianDone = { bold = true, fg = '#89ddff' },
+        ObsidianRightArrow = { bold = true, fg = '#f78c6c' },
+        ObsidianTilde = { bold = true, fg = '#ff5370' },
+        ObsidianBullet = { bold = true, fg = '#89ddff' },
+        ObsidianRefText = { underline = true, fg = '#c792ea' },
+        ObsidianExtLinkIcon = { fg = '#c792ea' },
+        ObsidianTag = { italic = true, fg = '#89ddff' },
+        ObsidianBlockID = { italic = true, fg = '#89ddff' },
+        ObsidianHighlightText = { bg = '#75662e' },
+      },
+    },
+  },
+}
