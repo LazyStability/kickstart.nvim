8c29d46aa9da08655ee5c3b16624785a3950fc04
LazyStability <Klysmus@online.de>
2024-09-12 16:21:45 +0200

    lspconfig without mason

--- lua/plugins/config/lspconfig.lua
+++ lua/plugins/config/lspconfig.lua
@@ -140,6 +140,7 @@ vim.api.nvim_create_autocmd('LspDetach', {
 --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+local on_attach = function(_, bufnr) end
 
 -- Enable the following language servers
 --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
@@ -150,67 +151,43 @@ capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp'
 --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
 --  - settings (table): Override the default settings passed when initializing the server.
 --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
+
 local servers = {
   clangd = {
     cmd = { 'clangd', '--query-driver=C:/ProgramData/mingw64/mingw64/bin/c++.exe' },
   },
-  -- gopls = {},
+  gopls = {},
   pyright = {},
   bashls = {},
-  -- rust_analyzer = {},
-  -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-  --
+  rust_analyzer = {},
+  --... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+
   -- Some languages (like typescript) have entire language plugins that can be useful:
-  --    https://github.com/pmizio/typescript-tools.nvim
-  --
+  -- https://github.com/pmizio/typescript-tools.nvim
+
   -- But for many setups, the LSP (`tsserver`) will work just fine
-  -- tsserver = {},
+  ts_ls = {},
 
   texlab = {},
-  -- nil_ls = {},
-  -- eslint = {},
-
+  nil_ls = {},
+  eslint = {},
   lua_ls = {
     -- cmd = {...},
-    -- filetypes = { ...},
-    -- capabilities = {},
+    -- filetype = {...},
     settings = {
       Lua = {
         completion = {
           callSnippet = 'Replace',
         },
-        -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-        -- diagnostics = { disable = { 'missing-fields' } },
       },
     },
   },
 }
 
--- Ensure the servers and tools above are installed
---  To check the current status of installed tools and/or manually install
---  other tools, you can run
---    :Mason
---
---  You can press `g?` for help in this menu.
-require('mason').setup()
-
--- You can add other tools here that you want Mason to install
--- for you, so that they are available from within Neovim.
-local ensure_installed = vim.tbl_keys(servers or {})
-vim.list_extend(ensure_installed, {
-  'stylua', -- Used to format Lua code
-})
-require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-
-require('mason-lspconfig').setup {
-  handlers = {
-    function(server_name)
-      local server = servers[server_name] or {}
-      -- This handles overriding only values explicitly passed
-      -- by the server configuration above. Useful when disabling
-      -- certain features of an LSP (for example, turning off formatting for tsserver)
-      server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-      require('lspconfig')[server_name].setup(server)
-    end,
-  },
-}
+-- Initialise all servers in the list
+for server_name in next, servers do
+  local server = servers[server_name] or {}
+  server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+  -- server.on_attach = vim.tbl_deep_extend('force', {}, on_attach, server.on_attach or {})
+  require('lspconfig')[server_name].setup(server)
+end
