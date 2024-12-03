cf5d30794c3b07a6b6b1b414472cf596a2d78193
LazyStability <Klysmus@online.de>
2024-12-04 00:55:40 +0100

    feat: go formatter

--- lua/plugins/core/conform.lua
+++ lua/plugins/core/conform.lua
@@ -27,6 +27,7 @@ return { -- Autoformat
     formatters_by_ft = {
       lua = { 'stylua' },
       nix = { 'alejandra' },
+      go = { 'gofmnt' },
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
       --
