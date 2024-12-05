2c4bb2389aaf70cae94974d2a9364ed8b36bb7df
LazyStability <Klysmus@online.de>
2024-12-05 17:27:17 +0100

    Revert "fix: treesitter nix"
    This reverts commit 849cab10c1abbb4b2b8bb8c8292c1ec1ba9a6dfc.

--- lua/plugins/core/treesitter.lua
+++ lua/plugins/core/treesitter.lua
@@ -2,12 +2,26 @@ return { -- Highlight, edit, and navigate code
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
   opts = {
-    ensure_installed = {},
+    ensure_installed = {
+      'bash',
+      'c',
+      'html',
+      'lua',
+      'luadoc',
+      'markdown_inline',
+      'markdown',
+      'vim',
+      'vimdoc',
+      'cpp',
+      'git_config',
+      'git_rebase',
+      'gitattributes',
+      'gitcommit',
+      'gitignore',
+    },
     -- Autoinstall languages that are not installed
     event = 'BufReadPost',
-    auto_install = false,
-    dev = true,
-    dir = '~/.local/share/nvim/nix-plugins/nvim-treesitter',
+    auto_install = true,
     highlight = {
       enable = true,
       disable = { 'latex' },
@@ -22,9 +36,9 @@ return { -- Highlight, edit, and navigate code
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 
     -- Prefer git instead of curl in order to improve connectivity in some environments
-    -- require('nvim-treesitter.install').prefer_git = true
-    -- ---@diagnostic disable-next-line: missing-fields
-    -- require('nvim-treesitter.configs').setup(opts)
+    require('nvim-treesitter.install').prefer_git = true
+    ---@diagnostic disable-next-line: missing-fields
+    require('nvim-treesitter.configs').setup(opts)
 
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
