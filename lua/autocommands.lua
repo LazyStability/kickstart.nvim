23fc11c6df0eb8739d27eefa79554638218bdd3c
LazyStability <Klysmus@online.de>
2024-12-13 17:23:58 +0100

    remove skeleton autocommands

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -11,24 +11,3 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
-
--- [[Basic Skeletons]]
--- See `:help skeletons`
-
--- When creating files with the mentioned extensions it will fill them with the template files
-local skeletons = vim.api.nvim_create_augroup('skeletons', { clear = true })
-vim.api.nvim_create_autocmd('BufNewFile', {
-  pattern = '*.tex',
-  group = skeletons,
-  command = '0r ~/Dokumente/Skeleton_files/LaTex/Root.tex',
-})
-vim.api.nvim_create_autocmd('BufNewFile', {
-  pattern = '*.h',
-  group = skeletons,
-  command = '0r ~/Dokumente/Skeleton_files/cpp/basic_header.h',
-})
-vim.api.nvim_create_autocmd('BufNewFile', {
-  pattern = '*.cpp',
-  group = skeletons,
-  command = '0r ~/Dokumente/Skeleton_files/cpp/basic_source_file.cpp',
-})
