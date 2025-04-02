f7ddcee58fe9563596b37988e098d593da03333b
LazyStability <Klysmus@online.de>
2025-04-02 12:38:04 +0200

    conform nixfmt

--- lua/plugins/core/conform.lua
+++ lua/plugins/core/conform.lua
@@ -26,7 +26,7 @@ return { -- Autoformat
     end,
     formatters_by_ft = {
       lua = { 'stylua' },
-      nix = { 'alejandra' },
+      nix = { 'nixfmt' },
       go = { 'gofmnt' },
       typst = { 'typstyle' },
       cpp = { 'clang-format' },
