2ca063cf5cf60029c021d0ade08ba918efed81c9
LazyStability <LazilyStable@proton.me>
2026-01-22 00:05:39 +0100

    feat(lsp): Add harper_ls

--- lua/plugins/config/lspconfig.lua
+++ lua/plugins/config/lspconfig.lua
@@ -223,6 +223,41 @@ local servers = {
       },
     },
   },
+  harper_ls = {
+    filetype = { 'typst', 'latex' },
+    settings = {
+      ['harper-ls'] = {
+        userDictPath = '',
+        workspaceDictPath = '',
+        fileDictPath = '',
+        linters = {
+          SpellCheck = true,
+          SpelledNumbers = false,
+          AnA = true,
+          SentenceCapitalization = true,
+          UnclosedQuotes = true,
+          WrongQuotes = false,
+          LongSentences = true,
+          RepeatedWords = true,
+          Spaces = true,
+          Matcher = true,
+          CorrectNumberSuffix = true,
+        },
+        codeActions = {
+          ForceStable = false,
+        },
+        markdown = {
+          IgnoreLinkTitle = false,
+        },
+        diagnosticSeverity = 'hint',
+        isolateEnglish = false,
+        dialect = 'British',
+        maxFileLength = 120000,
+        ignoredLintsPath = '',
+        excludePatterns = {},
+      },
+    },
+  },
   tinymist = {
     offset_encoding = 'utf-8',
     settings = {
