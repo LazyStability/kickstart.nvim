a49b4a441c2d940376ac1124c59f48f989867b13
LazyStability <LazilyStable@proton.me>
2026-05-03 13:39:11 +0200

    feat(telescope): Add file searcher for plugins

--- lua/plugins/config/telescope-fzf.lua
+++ lua/plugins/config/telescope-fzf.lua
@@ -37,6 +37,7 @@ M.config = function()
     -- },
     -- pickers = {}
     extensions = {
+      fzf = {},
       ['ui-select'] = {
         require('telescope.themes').get_dropdown(),
       },
@@ -59,6 +60,9 @@ M.config = function()
   vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
   vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
   vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+  vim.keymap.set('n', '<leader>se', function()
+    require('telescope.builtin').find_files { cwd = vim.fs.joinpath(vim.fn.stdpath 'data', 'lazy') }
+  end, { desc = 'Find plugin files' })
 
   -- Slightly advanced example of overriding default behavior and theme
   vim.keymap.set('n', '<leader>/', function()
